6e9ae9026d218ae2b2f9c8786c00b327cb4cd4a6
jonashohenstatter <jonashohenstatter@web.de>
2023-03-22 17:30:40 +0100

    Remove button + refresh button

--- src/App.js
+++ src/App.js
@@ -15,13 +15,21 @@ function App() {
         return hist.map(el => {
             const data = JSON.parse(el[1]);
             return {
-                date : el[0].split(" ").slice(0, -1).join("."),
+                date : el[0],
                 kcal : data.kcal,
                 protein : data.protein,
                 fat : data.fat,
                 carbs : data.carbs,
             }
-        })
+        }).reverse()
+    }
+
+    function removeEntry(key){
+        localStorage.removeItem(key);
+        setHistory(getHistory());
+        if(key === getDateString()){
+            clear()
+        }
     }
 
     function getDateString(){
@@ -46,12 +54,15 @@ function App() {
         setProtein(0);
         setFat(0);
         setCarbs(0);
+        console.log(getDateString())
+        localStorage.removeItem(getDateString());
+        setHistory(getHistory())
     }
 
     useEffect(() => {
         let dateString = getDateString();
         if(currentDate !== dateString){
-            localStorage.setItem(dateString,
+            localStorage.setItem(currentDate,
                 JSON.stringify({
                         "kcal" : kcal,
                         "protein" : protein,
@@ -60,6 +71,7 @@ function App() {
                     })
             )
             clear();
+            setDate(dateString)
         }
         if(load){
             try{
@@ -69,21 +81,30 @@ function App() {
             setLoad(false);
         }
         else{
-            localStorage.setItem(dateString,
-                JSON.stringify({
-                    "kcal" : kcal,
-                    "protein" : protein,
-                    "fat" : fat,
-                    "carbs" : carbs
-                }
-            ))
-            setHistory(getHistory());
+            if(kcal + protein + fat + carbs != 0){
+                localStorage.setItem(dateString,
+                    JSON.stringify({
+                            "kcal" : kcal,
+                            "protein" : protein,
+                            "fat" : fat,
+                            "carbs" : carbs
+                        }
+                    ))
+                setHistory(getHistory());
+            }
         }
 
     }, [kcal, protein, fat, carbs])
     return (
     <div className="bg-gray-800 min-h-screen min-w-screen">
-        <div className="grid md:grid-rows-1 md:grid-cols-none  md:grid-flow-col grid-rows-2 items-center md:p-10 p-5 grid-cols-2 gap-4 justify-center justify-items-center">
+        <div className="bg-gray-900 h-14 rounded-b-lg lg:mb-0 mb-7 drop-shadow-2xl grid grid-cols-3 justify-items-center items-center">
+            <div className=""></div>
+            <div className="text-white font-bold text-2xl flex ">150 kcal</div>
+            <button onClick={clear} className="text-white justify-self-end">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 fill-green-400 mr-5" viewBox="0 0 24 24"><path d="M13.5 2c-5.621 0-10.211 4.443-10.475 10h-3.025l5 6.625 5-6.625h-2.975c.257-3.351 3.06-6 6.475-6 3.584 0 6.5 2.916 6.5 6.5s-2.916 6.5-6.5 6.5c-1.863 0-3.542-.793-4.728-2.053l-2.427 3.216c1.877 1.754 4.389 2.837 7.155 2.837 5.79 0 10.5-4.71 10.5-10.5s-4.71-10.5-10.5-10.5z"/></svg>
+            </button>
+        </div>
+        <div className="grid md:grid-rows-1 md:grid-cols-none  md:grid-flow-col grid-rows-2 items-center md:p-10 pl-5 pr-5 grid-cols-2 gap-4 justify-center justify-items-center">
             <Counter name="kcal" unit="" initialIncrease={150} value={kcal} set={setKcal}></Counter>
             <Counter name="Protein" unit="g" initialIncrease={5} value={protein} set={setProtein}></Counter>
             <Counter name="Fat" unit="g" initialIncrease={5} value={fat} set={setFat}></Counter>
@@ -91,7 +112,7 @@ function App() {
         </div>
         <div className="flex justify-center items-center flex-col pl-3 pr-3 p-5 ">
             <div className="flex justify-center text-gray-200 text-4xl drop-shadow-2xl bg-gray-900 ml-5 mr-5 p-3 lg:w-2/4 w-full rounded-full">History </div>
-            <History history={history}></History>
+            <History history={history} removeEntry={removeEntry}></History>
         </div>
     </div>
   );
--- src/Components/History.js
+++ src/Components/History.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 
-function History({history}){
+function History({history, removeEntry}){
+
     return(
         <>
             <div className="mt-5 grid grid-cols-5 justify-items-center lg:w-2/4 lg:bg-none bg-gray-900 rounded-2xl w-full">
@@ -9,15 +10,18 @@ function History({history}){
                 <div className="lg:bg-gray-900 p-3 lg:pl-7 lg:pr-7 rounded-full text-white">Protein</div>
                 <div className="lg:bg-gray-900 p-3 lg:pl-7 lg:pr-7 rounded-full text-white">Fat</div>
                 <div className="lg:bg-gray-900 p-3 lg:pl-7 lg:pr-7 rounded-full text-white">Carbs</div>
-
             </div>
             {history.map( el => (
-                <div className="mt-5 grid grid-cols-5 justify-items-center lg:w-2/4 rounded-2xl w-full">
-                    <div className="text-white">{el.date}</div>
+                <div className="mt-5 grid grid-cols-5 justify-items-center lg:w-2/4 rounded-2xl w-full" key={el.date}>
+                    <div className="text-white">{el.date.split(" ").slice(0, -1).join(".")}</div>
                     <div className="text-white">{el.kcal}</div>
                     <div className="text-white">{el.protein}</div>
                     <div className="text-white">{el.fat}</div>
-                    <div className="text-white">{el.carbs}</div>
+                    <div className="text-white flex flex-row"><span className="ml-6 lg:ml-12">{el.carbs}</span>
+                        <button onClick={() => removeEntry(el.date)}><svg xmlns="http://www.w3.org/2000/svg" className="fill-red-400 w-5 h-5 ml-5 lg:ml-10 mb-3" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
+                    </button>
+                    </div>
+
                 </div>
             ))}
         </>
